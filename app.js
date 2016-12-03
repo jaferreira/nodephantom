109df4da59365efbdf94a0e23a90ac52f029ff94
João Ferreira <jaferrreira88@gmail.com>
2016-12-03 02:25:52 +0000

    FirstHorseman

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log('teste');
+var Horseman = require('node-horseman');
+var horseman = new Horseman();
+
+horseman
+  .open('http://www.abola.pt')
+
+  .count("li.g")
+  .then(function(numLinks){
+	console.log("Number of links: " +numLinks);
+	horseman.close();
+  });
