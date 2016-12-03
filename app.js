082846ba1e8c452c997bc974250215e84c597b1e
João Ferreira <jaferrreira88@gmail.com>
2016-12-03 02:28:43 +0000

    count li in abola

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var horseman = new Horseman();
 horseman
   .open('http://www.abola.pt')
 
-  .count("li.g")
+  .count("li")
   .then(function(numLinks){
 	console.log("Number of links: " +numLinks);
 	horseman.close();
