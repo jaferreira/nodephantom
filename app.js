d090f2ed6b61ef12646f9609ce98855af77ba94b
Luís Antunes <lr.antunes@gmail.com>
2016-12-07 15:12:55 +0000

    getTeamByName

--- app.js
+++ app.js
@@ -8,7 +8,35 @@ var app = express();
 
 var gameUrlsToScrap = [];
 
+function setPtTeamsInDB(){
+    var ptTeams = [
+        { "name": "Benfica", "url": "https://www.onlinebettingacademy.com/stats/team/portugal/benfica/1679" },
+        { "name": "Sporting CP", "url": "https://www.onlinebettingacademy.com/stats/team/portugal/sporting-cp/1680" },
+        { "name": "Porto", "url": "https://www.onlinebettingacademy.com/stats/team/portugal/porto/1678" },
+        { "name": "Sporting Braga", "url": "https://www.onlinebettingacademy.com/stats/team/portugal/sporting-braga/1682" },
+        { "name": "Vitória Guimarães", "url": "https://www.onlinebettingacademy.com/stats/team/portugal/vitria-guimares/1689" },
+        { "name": "Rio Ave", "url": "https://www.onlinebettingacademy.com/stats/team/portugal/rio-ave/1683" },
+        { "name": "Marítimo", "url": "https://www.onlinebettingacademy.com/stats/team/portugal/martimo/1684" },
+        { "name": "Chaves", "url": "https://www.onlinebettingacademy.com/stats/team/portugal/chaves/1704" },
+        { "name": "Estoril", "url": "https://www.onlinebettingacademy.com/stats/team/portugal/estoril/1695" },
+        { "name": "Belenenses", "url": "https://www.onlinebettingacademy.com/stats/team/portugal/belenenses/1692" },
+        { "name": "Arouca", "url": "https://www.onlinebettingacademy.com/stats/team/portugal/arouca/11611" },
+        { "name": "Paços de Ferreira", "url": "https://www.onlinebettingacademy.com/stats/team/portugal/paos-de-ferreira/1693" },
+        { "name": "Boavista", "url": "https://www.onlinebettingacademy.com/stats/team/portugal/boavista/1685" },
+        { "name": "Vitória Setúbal", "url": "https://www.onlinebettingacademy.com/stats/team/portugal/vitria-setbal/1696" },
+        { "name": "Moreirense", "url": "https://www.onlinebettingacademy.com/stats/team/portugal/moreirense/1687" },
+        { "name": "Feirense", "url": "https://www.onlinebettingacademy.com/stats/team/portugal/feirense/1705" },
+        { "name": "Tondela", "url": "https://www.onlinebettingacademy.com/stats/team/portugal/tondela/11833" },
+        { "name": "Nacional", "url": "https://www.onlinebettingacademy.com/stats/team/portugal/nacional/1681" }
+    ];
+
+    // TODO: Call db insert here...
+}
+
+
+
 function getTeamByName(name) {
+    // TODO: Call db find here...(by name)
     var ptTeams = [
         { "name": "Benfica", "url": "https://www.onlinebettingacademy.com/stats/team/portugal/benfica/1679" },
         { "name": "Sporting CP", "url": "https://www.onlinebettingacademy.com/stats/team/portugal/sporting-cp/1680" },
